73abdbfe86ac1d9914c370250099277f995cbe14
eumdengs <eumdengs@gmail.com>
2021-08-09 01:35:20 +0900

    done

--- src/js/todo.js
+++ src/js/todo.js
@@ -26,7 +26,7 @@ function paintToDo(text) {
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
-  delBtn.innerText = "✅";
+  delBtn.innerText = "done";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
